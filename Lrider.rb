409d42539061b905cb2209130eb2be04cc0c921b
rfccfreund <rmfreund@gmail.com>
2024-08-02 16:14:23 -0500

    Added second function for station queries

--- Lrider.rb
+++ Lrider.rb
@@ -7,17 +7,46 @@ require 'optparse'
 
 # CTA API information
 API_KEY = 'a20132c6acae4519aeef1f89f55a75cc'
-BASE_URL = 'http://lapi.transitchicago.com/api/1.0/ttpositions.aspx'
+BASE_URL = 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx'
 
 def fetch_train_routes()
-  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=40300&outputType=JSON")
+  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=41410&outputType=JSON")
   response = Net::HTTP.get(uri)
   data = JSON.parse(response)
   
 end
 
-result = fetch_train_routes()
+def fetch_station_info()
+  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=41410&outputType=JSON")
+  response = Net::HTTP.get(uri)
+  data = JSON.parse(response)
+  
+end
 
-puts result
-  
-  
+arrivals = fetch_train_routes()
+
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: cta_train_tracker.rb [options]"
+
+  opts.on("-l", "--line LINE", "Specify the train line (e.g., Red, Blue)") do |line|
+    options[:line] = line
+  end
+
+  opts.on("-s", "--station STATION_ID", "Specify the station ID") do |station_id|
+    options[:station_id] = station_id
+  end
+
+  opts.on("-h", "--help", "Displays Help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+# Ensure the station ID and line are provided
+if options[:station_id].nil? || options[:line].nil?
+  puts "Error: You must specify both a station ID and a train line."
+  puts "Use -h for help."
+  exit
+end
