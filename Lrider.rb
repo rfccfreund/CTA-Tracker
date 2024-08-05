8ae4f89988a1df4c1acbf0e28e22215022ab5faf
rfccfreund <rmfreund@gmail.com>
2024-08-05 18:20:05 -0500

    Updated line function to list all active runs

--- Lrider.rb
+++ Lrider.rb
@@ -4,27 +4,26 @@ require 'net/http'
 require 'json'
 require 'uri'
 require 'optparse'
+require 'pp'
 
 # CTA API information
 API_KEY = 'a20132c6acae4519aeef1f89f55a75cc'
 BASE_URL = 'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx'
+BASE_URL_LINES = 'http://lapi.transitchicago.com/api/1.0/ttpositions.aspx'
 
-def fetch_train_routes()
-  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=41410&outputType=JSON")
+def fetch_train_routes(line)
+  uri = URI("#{BASE_URL_LINES}?key=#{API_KEY}&rt=#{line}&outputType=JSON")
   response = Net::HTTP.get(uri)
   data = JSON.parse(response)
   
 end
 
-def fetch_station_info()
-  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=41410&outputType=JSON")
+def fetch_station_info(station)
+  uri = URI("#{BASE_URL}?key=#{API_KEY}&mapid=#{station}&outputType=JSON")
   response = Net::HTTP.get(uri)
   data = JSON.parse(response)
   
 end
-
-arrivals = fetch_train_routes()
-
 
 options = {}
 OptionParser.new do |opts|
@@ -32,6 +31,12 @@ OptionParser.new do |opts|
 
   opts.on("-l", "--line LINE", "Specify the train line (e.g., Red, Blue)") do |line|
     options[:line] = line
+    output = fetch_train_routes(line)
+    output.dig('ctatt', 'route', 0, 'train').each do |train|
+       pp "Run #{train.dig('rn')} is arriving at #{train.dig('nextStaNm')} at #{train.dig('arrT')}" 
+      
+    end      
+    
   end
 
   opts.on("-s", "--station STATION_ID", "Specify the station ID") do |station_id|
@@ -45,8 +50,8 @@ OptionParser.new do |opts|
 end.parse!
 
 # Ensure the station ID and line are provided
-if options[:station_id].nil? || options[:line].nil?
-  puts "Error: You must specify both a station ID and a train line."
-  puts "Use -h for help."
-  exit
-end
+# begin if options[:station_id].nil? && options[:line].nil?
+#   puts "Error: You must specify both a station ID and a train line."
+#   puts "Use -h for help."
+#   exit
+# end =end
